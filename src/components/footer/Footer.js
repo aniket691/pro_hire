83d91ab5887612f1d268ab8fadf6fd89b6c7ecf6
Aniket Dalal <58764738+aniket691@users.noreply.github.com>
2024-07-13 10:11:00 +0530

    made changes in footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,3 @@
-// src/Footer.js
 import React from 'react';
 import './Footer.css';
 
@@ -12,45 +11,45 @@ const Footer = () => {
                         JobBox is the heart of the design community and the best resource to discover and connect with designers and jobs worldwide.
                     </p>
                     <div className="footer-social">
-                        <a href="#"><i className="fab fa-facebook"></i></a>
-                        <a href="#"><i className="fab fa-twitter"></i></a>
-                        <a href="#"><i className="fab fa-linkedin"></i></a>
+                        <a href="https://www.facebook.com"><i className="fab fa-facebook"></i></a>
+                        <a href="https://www.twitter.com"><i className="fab fa-twitter"></i></a>
+                        <a href="https://www.linkedin.com"><i className="fab fa-linkedin"></i></a>
                     </div>
                 </div>
                 <div className="footer-column">
                     <h4>Resources</h4>
                     <ul>
-                        <li><a href="#">About us</a></li>
-                        <li><a href="#">Our Team</a></li>
-                        <li><a href="#">Products</a></li>
-                        <li><a href="#">Contact</a></li>
+                        <li><a href="/about-us">About us</a></li>
+                        <li><a href="/our-team">Our Team</a></li>
+                        <li><a href="/products">Products</a></li>
+                        <li><a href="/contact">Contact</a></li>
                     </ul>
                 </div>
                 <div className="footer-column">
                     <h4>Community</h4>
                     <ul>
-                        <li><a href="#">Feature</a></li>
-                        <li><a href="#">Pricing</a></li>
-                        <li><a href="#">Credit</a></li>
-                        <li><a href="#">FAQ</a></li>
+                        <li><a href="/feature">Feature</a></li>
+                        <li><a href="/pricing">Pricing</a></li>
+                        <li><a href="/credit">Credit</a></li>
+                        <li><a href="/faq">FAQ</a></li>
                     </ul>
                 </div>
                 <div className="footer-column">
                     <h4>Quick links</h4>
                     <ul>
-                        <li><a href="#">iOS</a></li>
-                        <li><a href="#">Android</a></li>
-                        <li><a href="#">Microsoft</a></li>
-                        <li><a href="#">Desktop</a></li>
+                        <li><a href="/ios">iOS</a></li>
+                        <li><a href="/android">Android</a></li>
+                        <li><a href="/microsoft">Microsoft</a></li>
+                        <li><a href="/desktop">Desktop</a></li>
                     </ul>
                 </div>
                 <div className="footer-column">
                     <h4>More</h4>
                     <ul>
-                        <li><a href="#">Privacy</a></li>
-                        <li><a href="#">Help</a></li>
-                        <li><a href="#">Terms</a></li>
-                        <li><a href="#">FAQ</a></li>
+                        <li><a href="/privacy">Privacy</a></li>
+                        <li><a href="/help">Help</a></li>
+                        <li><a href="/terms">Terms</a></li>
+                        <li><a href="/faq">FAQ</a></li>
                     </ul>
                 </div>
                 <div className="footer-column">
@@ -59,17 +58,17 @@ const Footer = () => {
                         Download our Apps and get extra 15% Discount on your first Order...!
                     </p>
                     <div className="footer-app-buttons">
-                        <a href="#"><img src="app-store.png" alt="App Store" /></a>
-                        <a href="#"><img src="google-play.png" alt="Google Play" /></a>
+                        <a href="https://www.apple.com/app-store/"><img src="app-store.png" alt="App Store" /></a>
+                        <a href="https://play.google.com/store"><img src="google-play.png" alt="Google Play" /></a>
                     </div>
                 </div>
             </div>
             <div className="footer-bottom">
                 <p>Copyright © 2022. JobBox all right reserved</p>
                 <ul>
-                    <li><a href="#">Privacy Policy</a></li>
-                    <li><a href="#">Terms & Conditions</a></li>
-                    <li><a href="#">Security</a></li>
+                    <li><a href="/privacy-policy">Privacy Policy</a></li>
+                    <li><a href="/terms-conditions">Terms & Conditions</a></li>
+                    <li><a href="/security">Security</a></li>
                 </ul>
             </div>
         </footer>
